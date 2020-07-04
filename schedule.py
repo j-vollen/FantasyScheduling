a20a4d73f38c361cfe60702385658b09f02006c3
Jeremy <jdvollen@gmail.com>
2020-07-05 01:28:16 +0200

    added divisional constraints

--- schedule.py
+++ schedule.py
@@ -2,12 +2,18 @@ import cvxpy as cp
 from itertools import combinations
 import numpy as np
 
+## DEFINE SCRIPT INPUTS
 # teams and number of games, respectively
 numTeams, numWeeks = 10, 13
 rivalry_week = 11
 # users names, rivalry matchups, and map from users to teams if needed
 users = np.array(["kevin", "jeremy", "robert", "miller", "simon", "joe", "greg", "justin", "sam", "chris"])
 rivalries = np.array([("kevin", "jeremy"), ("robert", "miller"), ("simon", "joe"), ("greg", "justin"), ("sam", "chris")])
+divisions = {
+    # First division
+    "kevin": 1,
+    # Second division
+    "jeremy": 2}
 user_team_map = {"kevin": "Professor Teabag", "chris": "1503 Sequoia Trail",
                  "justin": "bottom bitches", "robert": "Bulgogi", "greg": "Burrito House",
                  "simon": "Champ", "joe": "Hareem Kunt", "jeremy": "Hike School",
@@ -15,9 +21,6 @@ user_team_map = {"kevin": "Professor Teabag", "chris": "1503 Sequoia Trail",
 
 
 ## DEFINE DECISION VARIABLES
-# a decision variable for each unique matchup possibility in each week
-# games[i][j][k] indicates whether team i plays team j in week k
-# games = [[[cp.Variable(boolean=True) for i in range(n)] for j in range(n)] for k in range(m)]
 # dictionary with tuples (i,j) as keys and array of 13 cp variables as values
 games = {key: cp.Variable(numWeeks, boolean=True) for key in combinations(users, 2)}
 
@@ -40,5 +43,11 @@ spacing_constraints = [schedule[week]+schedule[week+1]+schedule[week+2] <= 1
                        for week in range(numWeeks-2) # looping over weeks
                        for schedule in games.values()] # looping over matchups' schedules
 
+# 4: division constraints (in-division play twice total; out-of-division play once total)
+division_constraints = \
+    [np.sum(vars) == 1 + (divisions[matchup[0]] == divisions[matchup[1]])
+     for matchup,vars in games.items()]
+
+## SOLVE
 # create problem, objective function is trivial
-#scheduling_problem = cp.Problem(cp.Maximize(0), [one_game_constraints, rivalry_constraints, spacing_constraints, division_constraints])
+scheduling_problem = cp.Problem(cp.Maximize(0), [one_game_constraints, rivalry_constraints, spacing_constraints, division_constraints])
