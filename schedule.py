ab4ee4296ad8cbc15315b400af859d73786400f6
jremedy <jdvollen@gmail.com>
2022-08-10 19:36:01 +1000

    fixed bug with inequality in division constraint; increased spacing constraint to 4 weeks; added rivalry week in week 1

--- schedule.py
+++ schedule.py
@@ -18,6 +18,7 @@ def pop_random(lst):
 # teams and number of games, respectively
 numTeams, numWeeks = 10, 14
 rivalry_week = 12
+rivalry_week2 = 1
 # users names, rivalry matchups, and map from users to teams if needed
 users = np.array(["kevin", "jeremy", "robert", "miller", "simon", "joe", "greg", "justin", "sam", "chris"])
 rivalries = [("jeremy", "greg"), ("robert", "joe"), ("kevin", "chris"), ("justin", "sam"), ("miller", "simon")]
@@ -36,14 +37,20 @@ while len(remaining_teams) > 1:
     tup = (rand1, rand2)
     ood_extra.append(tup)
 
-user_team_map = {"kevin": "Hare Club for Men", "chris": "Led Tasso",
-                 "justin": "Bottom Bitches", "robert": "Chicken adobo", "greg": "Smitty Werbenjagermanjensen",
-                 "simon": "ChuSerious", "joe": "Haywood Jablowme", "jeremy": "CCH Pounder",
-                 "sam": "Dumpster Fire", "miller": "Keith Urban"}
+user_team_map = {"kevin": "Hare Club for Men", 
+                "chris": "Led Tasso",
+                "justin": "Bottom Bitches", 
+                "robert": "Dave Savage Band",
+                "greg": "Smitty Werbenjagermanjensen",
+                "simon": "Adam Schefter",
+                "joe": "Haywood Jablowme",
+                "jeremy": "CCH Pounder",
+                "sam": "Dumpster Fire",
+                "miller": "Keith Urban"}
 
 
 # DEFINE DECISION VARIABLES
-# dictionary with tuples (i,j) as keys and array of 13 cp variables as values
+# dictionary with tuples (i,j) as keys and array of 14 cp variables as values
 games = {key: cp.Variable(numWeeks, boolean=True) for key in combinations(users, 2)}
 
 # CONSTRAINTS
@@ -61,28 +68,38 @@ for user in users:
 # 2: rivals must play each other in selected rivalry week;
 # note this will have bugs if the rivalry tuples are not in the lexicographic order of 'users' list
 rivalry_constraints = [games[rivalry][rivalry_week-1] == 1 for rivalry in rivalries]
+# added a second rivalry week. This didn't work last year.. I think it has to do with the number of rivalries are divisional
+rivalry_constraints2 = [games[rivalry][rivalry_week2-1] == 1 for rivalry in rivalries]
 
 # 3: game spacing constraints
 # maximum one game between any matchup of two teams in 3-week span
-spacing_constraints = [schedule[week]+schedule[week+1]+schedule[week+2] <= 1
-                       for week in range(numWeeks-2) # looping over weeks
+spacing_constraints = [schedule[week]+schedule[week+1]+schedule[week+2]+schedule[week+3] <= 1
+                       for week in range(numWeeks-3) # looping over weeks
                        for schedule in games.values()] # looping over matchups' schedules
 
 # 4: division constraints
 # in-division play twice total; out-of-division play once total except for those "extra" out-of-division matchups
+# made this an inequality, otherwise cvxpy says infeasible (even tho it is satisfied by equality in the solution)
 division_constraints = \
-    [cp.sum(weeks) == 1 + (divisions[matchup[0]] == divisions[matchup[1]]) + (matchup in ood_extra)
+    [cp.sum(weeks) >= 1 + (divisions[matchup[0]] == divisions[matchup[1]]) + (matchup in ood_extra)
      for matchup, weeks in games.items()]
 
-constraints = flatten([one_game_constraints, rivalry_constraints, spacing_constraints, division_constraints])
+constraints = flatten([one_game_constraints, rivalry_constraints, rivalry_constraints2, spacing_constraints, division_constraints])
 
 # SOLVE
 # create problem, objective function is trivial
 scheduling_problem = \
     cp.Problem(cp.Maximize(0),
                constraints)
-scheduling_problem.solve(solver='GLPK_MI')
+scheduling_problem.solve(solver='GLPK_MI', verbose=True)
 
+# debugging
+# total_ct = 0
+# for matchup, schedule in games.items():
+#     count = np.sum([schedule[week].value for week in range(numWeeks)])
+#     print(str(matchup) + ': ' + str(np.sum([schedule[week].value for week in range(numWeeks)])) + '\n')
+#     total_ct += count
+# print(str(total_ct) + '\n')
 
 # WRITE
 f = open('result.txt', 'r+')
@@ -99,5 +116,4 @@ for week in range(numWeeks):
 f.close()
 
 # To-dos:
-# For some reason, the program only finds a solution sometimes. Occasionally, it prints an empty solution. would be good to know why.. 
 # since there is no unique solution, add functionality so we can set a seed and get a different solution
